85328ca4f2bdb2ee5fb5e2e2ba998f1959f30d88
Pankajbohra <bohrapankajs@gmail.com>
2023-07-12 17:03:40 +0530

    test

--- vars.tf
+++ vars.tf
@@ -5,7 +5,7 @@ variable "PUBLIC_SUBNET_CIDR" {}
 variable "PRIVATE_SUBNET_CIDR" {}
 variable "AZ" {}
 variable "DEFAULT_VPC_ID" {}
-# variable "DEFAULT_VPC_CIDR" {}
+variable "DEFAULT_VPC_CIDR" {}
 # variable "DEFAULT_VPC_RT" {}
 
 # variable "PRIVATE_HOSTED_ZONEID" {}
